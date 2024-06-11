5d76bda7df9506a88d3de10aa3e9272a18c9c323
Crowdbotics <team@crowdbotics.com>
2024-06-11 06:36:18 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "peak-visory-48353",
+  url: "https://peak-visory-48353.botics.co",
+  api: "https://peak-visory-48353.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
